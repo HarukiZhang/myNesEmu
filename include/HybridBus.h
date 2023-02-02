49999d4cb341a45d9bfb0a68c2ce2c6eca4c6014
HarukiZhang <harukizhang@outlook.com>
2023-02-02 14:37:16 +0800

    modify: use function pointer to do the nt_mirror's works

--- include/HybridBus.h
+++ include/HybridBus.h
@@ -10,14 +10,20 @@ namespace nes {
         void connect(std::shared_ptr<Mapper> &_mapp);
         bool read(Word addr, Byte &data);
         bool write(Word addr, Byte data);
-        
+    
+    private:
+        Byte &mirror_horizontal(Word addr);
+        Byte &mirror_vertical(Word addr);
+        Byte &mirror_single_screen(Word addr);
+        Byte &mirror_four_screen(Word addr);
     private:
         VRAM vram;
         Palette palette;
 
         std::shared_ptr<Mapper> mapper = nullptr;
+        Byte &(*nt_mirror)(Word);
 
-        NT_Mirror nt_mirror;
+        // NT_Mirror nt_mirror; <-- no need any more;
     };
 
 
--- src/HybridBus.cpp
+++ src/HybridBus.cpp
@@ -3,7 +3,21 @@
 namespace nes {
     void HybridBus::connect(std::shared_ptr<Mapper> &_mapp){
         mapper = _mapp;
-        nt_mirror = mapper->get_nt_mirror();
+        switch (mapper->get_nt_mirror()){
+            case NT_Mirror::Horizontal :
+                nt_mirror = HybridBus::mirror_horizontal;
+                break;
+            case NT_Mirror::Vertical :
+                nt_mirror = HybridBus::mirror_vertical;
+                break;
+            case NT_Mirror::Single_Screen :
+                nt_mirror = HybridBus::mirror_single_screen;
+                break;
+            case NT_Mirror::Four_screen :
+                nt_mirror = HybridBus::mirror_four_screen;
+                break;
+            default: break;
+        }
     }
     
     bool HybridBus::read(Word addr, Byte &data){
@@ -11,62 +25,8 @@ namespace nes {
             return mapper->ppu_read(addr, data);
         }
         else if (addr < 0x3000){
-            switch (nt_mirror){
-            case NT_Mirror::Horizontal :
-                {
-                    // 0010 0000 0000 0000 -> $0000
-                    // 0010 0011 1111 1111 -> $03FF
-                    // 0010 0100 0000 0000 -> $0000
-                    // 0010 0111 1111 1111 -> $03FF
-                    // 0010 1000 0000 0000 -> $0400
-                    // 0010 1011 1111 1111 -> $07FF
-                    // 0010 1100 0000 0000 -> $0400
-                    // 0010 1111 1111 1111 -> $07FF
-                    bool coef = addr & 0x800;
-                    data = vram[(coef * 0x400) | (addr & 0x3ff)];
-                    return true;
-                }
-                break;
-            case NT_Mirror::Vertical :
-                {
-                    // 0010 0000 0000 0000 -> $0000
-                    // 0010 0011 1111 1111 -> 0000 0011 1111 1111
-                    // 0010 0100 0000 0000 -> 0000 0100 0000 0000
-                    // 0010 0111 1111 1111 -> 0000 0111 1111 1111
-                    // 0010 1000 0000 0000 -> $0000
-                    // 0010 1011 1111 1111 -> 0000 0011 1111 1111
-                    // 0010 1100 0000 0000 -> 0000 0100 0000 0000
-                    // 0010 1111 1111 1111 -> 0000 0111 1111 1111
-                    data = vram[addr & 0x7ff];
-                    return true;
-                }
-                break;
-            case NT_Mirror::Single_Screen :
-                {
-                    data = vram[addr & 0x3ff];
-                    return true;
-                }
-                break;
-            case NT_Mirror::Four_screen :
-                {
-                    if (addr & 0x800){
-                        //high addr need to call mapper;
-                        
-                        //TODO: how to address cart-nested ram?
-                        
-                        return false;
-                    }
-                    else {
-                        data = vram[addr & 0x7ff];
-                        return true;
-                    }
-                }
-                break;
-            default :
-                //other NameTable Mirroring is not suppurted;
-                return false;
-                break;
-            }
+            data = (this->*nt_mirror)(addr);
+            return true;
         }
         else {// addr < 0x4000;
             //assume that user wont address $3000 - $3EFF;
@@ -80,46 +40,8 @@ namespace nes {
             return mapper->ppu_write(addr, data);
         }
         else if (addr < 0x3000){
-            switch (nt_mirror){
-            case NT_Mirror::Horizontal :
-                {
-                    bool coef = addr & 0x800;
-                    vram[(coef * 0x400) | (addr & 0x3ff)] = data;
-                    return true;
-                }
-                break;
-            case NT_Mirror::Vertical :
-                {
-                    vram[addr & 0x7ff] = data;
-                    return true;
-                }
-                break;
-            case NT_Mirror::Single_Screen :
-                {
-                    vram[addr & 0x3ff] = data;
-                    return true;
-                }
-                break;
-            case NT_Mirror::Four_screen :
-                {
-                    if (addr & 0x800){
-                        //high addr need to call mapper;
-                        
-                        //TODO: how to address cart-nested ram?
-                        
-                        return false;
-                    }
-                    else {
-                        vram[addr & 0x7ff] = data;
-                        return true;
-                    }
-                }
-                break;
-            default :
-                //other NameTable Mirroring is not suppurted;
-                return false;
-                break;
-            }
+            (this->*nt_mirror)(addr) = data;
+            return true;
         }
         else {// addr < 0x4000;
             //assume that user wont address $3000 - $3EFF;
@@ -127,4 +49,45 @@ namespace nes {
             return true;
         }
     }
+
+    //mirror functions =========================================================
+    Byte &HybridBus::mirror_horizontal(Word addr){
+        // 0010 0000 0000 0000 -> $0000
+        // 0010 0011 1111 1111 -> $03FF
+        // 0010 0100 0000 0000 -> $0000
+        // 0010 0111 1111 1111 -> $03FF
+        // 0010 1000 0000 0000 -> $0400
+        // 0010 1011 1111 1111 -> $07FF
+        // 0010 1100 0000 0000 -> $0400
+        // 0010 1111 1111 1111 -> $07FF
+        bool coef = addr & 0x800;
+        return vram[(coef * 0x400) | (addr & 0x3ff)];
+    }
+
+    Byte &HybridBus::mirror_vertical(Word addr){
+        // 0010 0000 0000 0000 -> $0000
+        // 0010 0011 1111 1111 -> 0000 0011 1111 1111
+        // 0010 0100 0000 0000 -> 0000 0100 0000 0000
+        // 0010 0111 1111 1111 -> 0000 0111 1111 1111
+        // 0010 1000 0000 0000 -> $0000
+        // 0010 1011 1111 1111 -> 0000 0011 1111 1111
+        // 0010 1100 0000 0000 -> 0000 0100 0000 0000
+        // 0010 1111 1111 1111 -> 0000 0111 1111 1111
+        return vram[addr & 0x7ff];
+    }
+
+    Byte &HybridBus::mirror_single_screen(Word addr){
+        return vram[addr & 0x3ff];
+    }
+
+    Byte &HybridBus::mirror_four_screen(Word addr){
+        if (addr & 0x800){
+            //high addr need to call mapper;
+            //TODO: how to address cart-nested ram?
+            return vram[addr & 0x7ff];//  <--- this is wrong now;
+        }
+        else
+            return vram[addr & 0x7ff];
+    }
+
 };//end nes
